57e93b713d097f3876eb51ebe941cd62873c6cd1
Utsav Sharma <utsav2341@gmail.com>
2020-05-14 22:38:39 +0530

    Add error controller

--- src/main/java/com/bookstore/springboot/books/rest/MyErrorController.java
+++ src/main/java/com/bookstore/springboot/books/rest/MyErrorController.java
@@ -14,7 +14,9 @@ public class MyErrorController implements ErrorController  {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         
-    	Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+            System.out.println("Error controller");
+
+	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
