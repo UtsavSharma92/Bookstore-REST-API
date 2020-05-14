ccd9ff4e5cfc0000877be1c924913f34d2a58e0f
Utsav Sharma <utsav2341@gmail.com>
2020-05-14 21:57:10 +0530

    Printing book id on console

--- src/main/java/com/bookstore/springboot/books/service/BookServiceImpl.java
+++ src/main/java/com/bookstore/springboot/books/service/BookServiceImpl.java
@@ -28,7 +28,9 @@ public class BookServiceImpl implements BookService {
 	@Override
 	public Books findById(int isbn) {
 		Optional<Books> result = booksRepository.findById(isbn);
-
+              
+		System.out.println("book id :"+isbn);
+		
 		Books books = null;
 
 		if (result.isPresent())
